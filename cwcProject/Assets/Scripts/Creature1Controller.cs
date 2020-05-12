e3b9472e9adacb1368fc12d481852559065194c8
Jacob Valero <54868391+Orelavant@users.noreply.github.com>
2020-05-12 12:55:49 -0600

    Added project

---
+++ cwcProject/Assets/Scripts/Creature1Controller.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature1Controller : MonoBehaviour {
+    // TODO CURRENT ISSUE WHEN TWO PLAYERS COLLIDE, VELOCITY DOESN'T RESET FOR FLYING GUY.
+
+    // Control fields
+    private float horizontalInput;
+    private float verticalInput;
+    public float speed = 10f;
+
+    // Location fields
+    public float xBounds = 15f;
+    public float yUpperBounds = 20f;
+    public float yLowerBounds = 0f;
+
+    // Physics
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start() {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Movement
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        // Keep the player in bounds
+        if (transform.position.x < -xBounds) {
+            transform.position = new Vector3(-xBounds, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xBounds) {
+            transform.position = new Vector3(xBounds, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y > yUpperBounds) {
+            transform.position = new Vector3(transform.position.x, yUpperBounds, transform.position.z);
+        }
+        if (transform.position.y < yLowerBounds) {
+            transform.position = new Vector3(transform.position.x, yLowerBounds, transform.position.z);
+        }
+
+        // Cancel out added velocity from bumping
+
+    }
+}
---
+++ cwcProject/Assets/Scripts/Creature2Controller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature2Controller : MonoBehaviour
+{
+    // Control fields
+    private float horizontalInput;
+    public float jumpForce = 10f;
+    public float speed = 10f;
+
+    // Location fields
+    public bool onGround;
+    public float bounds = 15;
+    
+    // Physics
+    private Rigidbody playerRb;
+    public float gravityModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        // Keep the player in bounds
+        if (transform.position.x < -bounds) {
+            transform.position = new Vector3(-bounds, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > bounds) {
+            transform.position = new Vector3(bounds, transform.position.y, transform.position.z);
+        }
+
+        // Jump
+        if (Input.GetKeyDown("space") && onGround == true) {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            onGround = false;
+        } 
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            onGround = true;
+        }
+    }
+}
